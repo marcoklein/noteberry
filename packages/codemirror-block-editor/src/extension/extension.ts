e81bb55f43657d4aee300e8bb5acb7af3edf813a
Marco Klein <marco.klein94@gmail.com>
2022-03-03 20:28:45 +0100

    feat: add dot view

--- packages/codemirror-block-editor/src/extension/extension.ts
+++ packages/codemirror-block-editor/src/extension/extension.ts
@@ -1,10 +1,6 @@
-import { ChangeSpec, EditorState, Extension } from "@codemirror/state";
+import { Extension } from "@codemirror/state";
 import { addBlockOnNewLine } from "./add-block-on-new-line";
 import { dotWidgetViewPlugin } from "./dot-widget";
-import {
-  findBlockLevelCharacterIndentationOfLine,
-  findBlockLevelOfLineNumberInDocument,
-} from "./find-block-level-of-line";
 import { handleChangeWithinBlockLevel } from "./handle-change-within-block-level";
 import { blockLevelKeymap } from "./keymap";
 import { dotTheme } from "./theme";
@@ -13,7 +9,7 @@ import { validateCursorPosition } from "./validate-cursor-position";
 export function blockLevelExtension(_options: {} = {}): Extension {
   return [
     // TODO there is a bug that changes precedence -> have to change order of extensions with that patch
-    // dotWidgetViewPlugin,
+    dotWidgetViewPlugin,
 
     validateCursorPosition,
     addBlockOnNewLine,
