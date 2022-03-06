b6b0dc8234fb7e5e1f4d2b54d52895c147c84685
Marco Klein <marco.klein94@gmail.com>
2022-03-06 23:21:22 +0100

    feat: expose block marker facet

--- packages/codemirror-block-editor/src/index.ts
+++ packages/codemirror-block-editor/src/index.ts
@@ -1,4 +1,5 @@
 export { blockEditor } from "./extension/extension";
 export { blockLevelListenerFacet } from "./extension/block-level-listener";
 export * from "./extension/set-block-level-effect";
+export * from "./extension/block-marker-facet";
 export * from "./version.gen";
